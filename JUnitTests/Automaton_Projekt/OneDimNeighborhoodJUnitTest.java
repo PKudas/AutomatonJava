da330e66ed6fa1a17202b43acf75ee21b29366f1
Patryk <Patryk@Patryk>
2016-01-06 15:57:33 +0100

    Commit Automat

---
+++ JUnitTests/Automaton_Projekt/OneDimNeighborhoodJUnitTest.java
@@ -0,0 +1,36 @@
+package Automaton_Projekt;
+
+
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Patryk
+ */
+public class OneDimNeighborhoodJUnitTest {
+    
+    @Test
+    public void NeighborsWithWrappingInTheMiddle() {
+        int size = 4;
+        boolean wrap = true;
+	Set<CellCoordinates> neighbors = new HashSet();
+        neighbors.add(new Coords1D(0));
+        neighbors.add(new Coords1D(2));
+        CellNeighborhood neigh = new OneDimNeighborhood(size,wrap);
+        assertEquals(neighbors,neigh.cellNeighbors(new Coords1D(1)));
+    }
+    @Test
+    public void NeighborsWithWrappingOnTheEdge() {
+        int size = 4;
+        boolean wrap = true;
+	Set<CellCoordinates> neighbors = new HashSet();
+        neighbors.add(new Coords1D(3));
+        neighbors.add(new Coords1D(1));
+        CellNeighborhood neigh = new OneDimNeighborhood(size,wrap);
+        assertEquals(neighbors,neigh.cellNeighbors(new Coords1D(0)));
+    }
+}
